efaf9bfabd46864e554321ddbf221da2ab7db824
emma-campbell <emma@emmacampbell.io>
2019-07-11 16:05:35 -0400

    adding authorization

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 // src/server.ts
 import app from './app';
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log('Server listening on http://localhost:' + PORT);
 });
+
