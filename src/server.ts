d649bf24fdd8cf078d2f7624adfafd5d8a0f6e39
emma-campbell <emma@emmacampbell.io>
2019-07-22 13:24:14 -0400

    server runs

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,55 @@
 import app from "./app";
+import debug from "debug";
+import * as http from "http";
 import "dotenv/config";
 
 const {
     HOST,
-    PORT
+    PORT,
+    NODE_ENV
 } = process.env;
 
-const server = app.listen(PORT, () => {
-    console.log("Server now listening on http://" + HOST + ":" + PORT);
-    console.log("   PRESS CTRL-C TO STOP");
-});
+if (NODE_ENV !== 'production') {
+    debug('ts-express:server');
+}
 
+const port = PORT || 8080; 
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log("Listening on http://" + HOST + ":" + PORT);
+console.log("    PRESS CTRL-C TO STOP");
+
+function normalizePort(val: number|string): number|string|boolean {
+    let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
+    if (isNaN(port)) return val;
+    else if (port >= 0) return port;
+    else return false;
+}
+
+function onError(error: NodeJS.ErrnoException): void {
+    if (error.syscall !== 'listen') throw error;
+    let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+    switch(error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening(): void {
+    let addr = server.address();
+    let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+    debug(`Listening on ${bind}`);
+}
 export default server;
